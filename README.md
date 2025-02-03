a106ee9bcae09be0905b68dd9f1ad403e8f3a201
kerenel <kerenel@outlook.com>
2025-02-03 22:14:34 +0200

    initial:commit

---
+++ README.md
@@ -0,0 +1,118 @@
+# cprintf
+
+`cprintf` is a header-only C library that adds ANSI colours and attributes to `printf` without having to write the full code out. This makes it easy to add coloured and styled text to your terminal output.
+
+## Features
+
+- Header-only library
+- Supports ANSI colours for foreground and background
+- Supports various text attributes
+- Easy integration with existing code
+
+---
+
+## INSTALLATION
+
+Simply include the `cprintf.h` header file in your project.
+
+---
+
+## USAGE
+
+To use `cprintf`, define `CPRINTF_IMPLEMENTATION` before including the header file in one of your source files.
+
+```C
+#define CPRINTF_IMPLEMENTATION
+#include "cprintf.h"
+```
+
+Then, you can use cprintf in your code just like printf, but with additional formatting options.
+
+---
+
+## EXAMPLE
+
+```C
+#define CPRINTF_IMPLEMENTATION
+#include "cprintf.h"
+
+int main(int argc, char * argv[]) {
+    cprintf("%FbBLUE TEXT%Ar\n");
+    return 0;
+}
+```
+
+---
+
+## FORMATING OPTIONS
+
+
+### FOREGROUND COLOURS
+
+- **%Fk**: Black
+
+- **%Fr**: Red
+
+- **%Fg**: Green
+
+- **%Fy**: Yellow
+
+- **%Fb**: Blue
+
+- **%Fm**: Magenta
+
+- **%Fc**: Cyan
+
+- **%Fw**: White
+
+### BACKGROUND COLOURS
+
+- **%Bk**: Black
+
+- **%Br**: Red
+
+- **%Bg**: Green
+
+- **%By**: Yellow
+
+- **%Bb**: Blue
+
+- **%Bm**: Magenta
+
+- **%Bc**: Cyan
+
+- **%Bw**: White
+
+### ATTRIBUTES
+
+- **%Ar**: Reset
+
+- **%Ab**: Bold
+
+- **%Af**: Dim
+
+- **%Ai**: Italic
+
+- **%Au**: Underline
+
+- **%Al**: Blink (slow)
+
+- **%Ak**: Blink (rapid)
+
+- **%Av**: Inverted
+
+- **%Ah**: Hidden
+
+- **%As**: Strikethrough
+
+---
+
+## LICENSE
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+## CONTRIBUTING
+
+Feel free to submit issues and pull requests. Contributions are welcome!
---
+++ cprintf.h
@@ -0,0 +1,138 @@
+#ifndef CPRINTF_H
+#define CPRINTF_H
+
+#ifdef CPRINTF_IMPLEMENTATION
+
+#define CPRINTF_BLACK                               0
+#define CPRINTF_RED                                 1
+#define CPRINTF_GREEN                               2
+#define CPRINTF_YELLOW                              3
+#define CPRINTF_BLUE                                4
+#define CPRINTF_MAGENTA                             5
+#define CPRINTF_CYAN                                6
+#define CPRINTF_WHITE                               7
+#define CPRINTF_DEFAULT                             9
+
+#define CPRINTF_RESET                               0
+#define CPRINTF_BOLD                                1
+#define CPRINTF_DIM                                 2
+#define CPRINTF_ITALIC                              3
+#define CPRINTF_UNDERLINE                           4
+#define CPRINTF_BLINK_SLOW                          5
+#define CPRINTF_BLINK_RAPID                         6
+#define CPRINTF_INVERTED                            7
+#define CPRINTF_HIDDEN                              8
+#define CPRINTF_STRIKETHOUGH                        9
+
+#define CPRINTF_BUFFER_SIZE                         2048
+
+static const char * CPRINTF_FG_COLOURS[] = {
+    "\033[30m",
+    "\033[31m",
+    "\033[32m",
+    "\033[33m",
+    "\033[34m", 
+    "\033[35m",
+    "\033[36m",
+    "\033[37m"
+};
+
+static const char * CPRINTF_BG_COLOURS[] = {
+    "\033[40m",
+    "\033[41m",
+    "\033[42m",
+    "\033[43m",
+    "\033[44m", 
+    "\033[45m",
+    "\033[46m",
+    "\033[47m"
+};
+
+static const char * CPRINTF_ATTRIBUTES[] = {
+    "\033[0m",
+    "\033[1m",
+    "\033[2m",
+    "\033[3m",
+    "\033[4m",
+    "\033[5m", 
+    "\033[6m",
+    "\033[7m",
+    "\033[8m",
+    "\033[9m"
+};
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+int cprintf(const char * restrict format, ...) {
+    va_list args;
+    va_start(args, format);
+    char buffer[CPRINTF_BUFFER_SIZE];
+    int index = 0;
+    const char * pointer = format;
+
+    while (pointer[0]) {
+        if (pointer[0] == '%' && (pointer[1] == 'F' || pointer[1] == 'B' || pointer[1] == 'A')) {
+            pointer++;
+
+            switch (pointer[0]) {
+                case 'F':  // FOREGROUND COLOR
+                case 'B':  // BACKGROUND COLOR
+                    pointer++;
+
+                    switch (pointer[0]) {
+                        case 'k': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_BLACK]   : CPRINTF_BG_COLOURS[CPRINTF_BLACK]);    break;
+                        case 'r': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_RED]     : CPRINTF_BG_COLOURS[CPRINTF_RED]);      break;
+                        case 'g': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_GREEN]   : CPRINTF_BG_COLOURS[CPRINTF_GREEN]);    break;
+                        case 'y': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_YELLOW]  : CPRINTF_BG_COLOURS[CPRINTF_YELLOW]);   break;
+                        case 'b': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_BLUE]    : CPRINTF_BG_COLOURS[CPRINTF_BLUE]);     break;
+                        case 'm': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_MAGENTA] : CPRINTF_BG_COLOURS[CPRINTF_MAGENTA]);  break;
+                        case 'c': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_CYAN]    : CPRINTF_BG_COLOURS[CPRINTF_CYAN]);     break;
+                        case 'w': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", (pointer[-1] == 'F') ? CPRINTF_FG_COLOURS[CPRINTF_WHITE]   : CPRINTF_BG_COLOURS[CPRINTF_WHITE]);    break;
+                    }
+
+                    index += strlen(&buffer[index]);
+                    break;
+                case 'A':  // ATTRIBUTES
+                    pointer++;
+
+                    switch (*pointer) {
+                        case 'r': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_RESET]); break;
+                        case 'b': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_BOLD]); break;
+                        case 'f': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_DIM]); break;
+                        case 'i': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_ITALIC]); break;
+                        case 'u': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_UNDERLINE]); break;
+                        case 'l': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_BLINK_SLOW]); break;
+                        case 'k': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_BLINK_RAPID]); break;
+                        case 'v': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_INVERTED]); break;
+                        case 'h': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_HIDDEN]); break;
+                        case 's': snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%s", CPRINTF_ATTRIBUTES[CPRINTF_STRIKETHOUGH]); break;
+                    }
+
+                    index += strlen(&buffer[index]);
+                    break;
+                default:
+                    snprintf(&buffer[index], CPRINTF_BUFFER_SIZE - index, "%%%c", *pointer);
+                    index += strlen(&buffer[index]);
+                    break;
+            }
+        } else {
+            buffer[index++] = *pointer;
+        }
+
+        if (index >= CPRINTF_BUFFER_SIZE) {
+            break;  // STOP IF BUFFER SIZE OVERFLOWS
+        }
+        
+        pointer++;
+    }
+
+    buffer[index] = '\0';
+    vprintf(buffer, args);
+    va_end(args);
+}
+
+#endif // CPRINTF_IMPLEMENTATION
+
+#endif // CPRINF_H
